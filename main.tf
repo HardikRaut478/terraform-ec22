9a4a5d3947a9e7000fce447f6eac45522f86f281
Hardik Raut <155015767+HardikRaut478@users.noreply.github.com>
2025-09-09 23:47:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "my_ec2" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  key_name      = var.key_name  # Make sure you pass your key pair name here
+  key_name      = Key (1)  # Make sure you pass your key pair name here
 
   # Install Docker at launchdfgdfg
   user_data = <<-EOF
